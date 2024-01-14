f31b44f2cd979012ec9e516357148b199b35ea96
Matthew Burgess <boog397@gmail.com>
2024-01-13 20:52:30 -0600

    Add function to play a single round

--- javascript.js
+++ javascript.js
@@ -1,4 +1,36 @@
-console.log(getComputerChoice());
+const playerSelection = prompt("Rock, paper, or scissors?");
+
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return "Tie! Both players played " + computerSelection + ".";
+    } else if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection.toLowerCase() === "paper") {
+            return "You lose! Paper beats Rock";
+        } else {
+            return "You win! Rock beats Scissors";
+        }
+    } else if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection.toLowerCase() === "scissors") {
+            return "You lose! Scissors beats Paper";
+        } else {
+            return "You win! Paper beats Rock";
+        }
+    } else if (playerSelection.toLowerCase() === "scissors") {
+        if (computerSelection.toLowerCase() === "rock") {
+            return "You lose! Rock beats Scissors";
+        } else {
+            return "You win! Scissors beats Paper";
+        }
+    } else {
+        return playerSelection + " isn't a valid option. Please try again.";
+    }
+}
 
 function getComputerChoice() {
     let options = ["Rock", "Paper", "Scissors"]
